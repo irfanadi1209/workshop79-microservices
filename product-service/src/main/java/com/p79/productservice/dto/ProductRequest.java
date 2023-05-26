8a6a856de69d73a949cf11a1c1eba7b9c84c8370
luthfimaajid <luthfimaajid@gmail.com>
2023-05-26 21:16:27 +0700

    ignore

---
+++ product-service/src/main/java/com/p79/productservice/dto/ProductRequest.java
@@ -0,0 +1,15 @@
+package com.p79.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class ProductRequest {
+    private String name;
+    private String description;
+}
